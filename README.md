cce9ecddf389310464fc8bdfd843cb22d5cdf94d
Matata <matias.agf@gmail.com>
2020-09-26 12:04:06 -0300

    arreglado el mapa y los markes traidos de la API y puestos con forEach

--- README.md
+++ README.md
@@ -1,1 +1,2 @@
 # courseranode
+# Link al repositorio privado: https://github.com/MatataFerra/courseranode.git
--- public/javascripts/map.js
+++ public/javascripts/map.js
@@ -3,22 +3,36 @@ mapboxgl.accessToken = 'pk.eyJ1IjoibWF0YXRhZmVycmEiLCJhIjoiY2tlbTczeWk3MGdoNDMyb
 var map = new mapboxgl.Map({
 container: 'map',
 style: 'mapbox://styles/mapbox/streets-v11',
-center: [-58.488043, -34.568419], // starting position [lng, lat]
+center: [-58.3861497, -34.6012424], // starting position [lng, lat]
 zoom: 13 // starting zoom
 });
 
-var marker = new mapboxgl.Marker()
-  .setLngLat([-58.488043, -34.568419])
-  .addTo(map);
+// var marker = new mapboxgl.Marker()
+//     .setLngLat([-58.488043, -34.568419])
+//     .addTo(map);
 
-var marker2 = new mapboxgl.Marker()
-  .setLngLat([-58.386149, -34.601242])
-  .addTo(map);
+// var marker2 = new mapboxgl.Marker()
+//     .setLngLat([-58.386149, -34.601242])
+//     .addTo(map);
 
-var marker3 = new mapboxgl.Marker()
-  .setLngLat([-58.480828, -34.596932])
-  .addTo(map);
+// var marker3 = new mapboxgl.Marker()
+//     .setLngLat([-58.480828, -34.596932])
+//     .addTo(map);
 
+const bicisFetch = fetch("api/bicicletas")
+.then(res => {
+    
+    return res.json();
+})
+.then(data=>{
+    console.log(data)
+    data.bicicletas.forEach(bici => {
+        console.log(bici.ubicacion)
+        new mapboxgl.Marker()
+        .setLngLat(bici.ubicacion)
+        .addTo(map)
+    });
+})
 // $.ajax({
 //     dataType: "json",
 //     url: "api/bicicletas",
