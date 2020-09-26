506ec15f4854cf8a6b052c29f6a94fbfbca0af32
Matata <matias.agf@gmail.com>
2020-09-26 12:10:02 -0300

    agregada info al readme

--- README.md
+++ README.md
@@ -1,2 +1,8 @@
 # courseranode
 # Link al repositorio privado: https://github.com/MatataFerra/courseranode.git
+
+# Api endpoints: 
+
+# /api/bicicletas ->obtener todas las bicicletas
+# /api/bicicletas/create -> crear una bicicleta
+# /api/bicicletas/delete -> borrar una bicicleta
--- public/javascripts/map.js
+++ public/javascripts/map.js
@@ -7,17 +7,7 @@ center: [-58.3861497, -34.6012424], // starting position [lng, lat]
 zoom: 13 // starting zoom
 });
 
-// var marker = new mapboxgl.Marker()
-//     .setLngLat([-58.488043, -34.568419])
-//     .addTo(map);
 
-// var marker2 = new mapboxgl.Marker()
-//     .setLngLat([-58.386149, -34.601242])
-//     .addTo(map);
-
-// var marker3 = new mapboxgl.Marker()
-//     .setLngLat([-58.480828, -34.596932])
-//     .addTo(map);
 
 const bicisFetch = fetch("api/bicicletas")
 .then(res => {
@@ -33,15 +23,3 @@ const bicisFetch = fetch("api/bicicletas")
         .addTo(map)
     });
 })
-// $.ajax({
-//     dataType: "json",
-//     url: "api/bicicletas",
-//     success: function(result) {
-//         console.log(result);
-//         result.bicicletas.forEach( bici => {
-//             var marker = new mapboxgl.Marker()
-//             .setLngLat([bici.ubicacion])
-//             .addTo(map);
-//         })
-//     }
-// })  PETICION A LA API y hacer un foreach para cada marcador
